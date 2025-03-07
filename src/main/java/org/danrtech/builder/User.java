4c234ac61eee186c39695eb912045e780287d3d7
danr <danilireland@gmail.com>
2025-03-07 12:21:53 +0000

    Builder pattern
    Initial commit that has the Builder Pattern example in the builder/User.java class.

---
+++ src/main/java/org/danrtech/builder/User.java
@@ -0,0 +1,37 @@
+package org.danrtech.builder;
+
+public class User {
+    private final String name;
+    private final int age;
+
+    private User(Builder builder) {
+        this.name = builder.name;
+        this.age = builder.age;
+    }
+
+    public static void main(String[] args){
+        User usr = new Builder().age(5).name("Alex").build();
+        System.out.println(usr.age);
+        System.out.println(usr.name);
+    }
+
+
+    public static class Builder {
+        private String name;
+        private int age;
+
+        public Builder name(String name) {  // No need for "set" prefix
+            this.name = name;
+            return this;
+        }
+
+        public Builder age(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
+    }
+}
